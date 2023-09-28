bbaf92a3dc17368167fdb499a4b9e5c676d2d2d5
Angel Stoychev <122397062+AngelStch@users.noreply.github.com>
2023-09-28 23:07:17 +0300

    Add files via upload
    some upgrades
    ;)

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const http = require("http")
-const homeHtml = require("./views/home/index.js")
-const siteCss = require("./content/styles/site.js")
-const addBread = require("./views/addBreed.js")
-const addCat = require("./views/addCat.js")
-const catTemplate = require(`./views/home/catTemplte.js`)
+const fs = require("fs/promises")
+
 const port = 5050
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     const { url } = req
 
     const cats = [
@@ -17,21 +14,21 @@ const server = http.createServer((req, res) => {
 
         },
         {
-            imageUrl: '',
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw4xHiYs4vnhBs9jqjYk0_JY3-SiSavqovXA&usqp=CAU",
             name: "Tsunami",
             breed: "ulichna2",
             description: "Very Cute cat!2",
 
         },
         {
-            imageUrl: '',
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw4xHiYs4vnhBs9jqjYk0_JY3-SiSavqovXA&usqp=CAU",
             name: "Mariq",
             breed: "ulichna3",
             description: "Very Cute cat!3",
 
         },
         {
-            imageUrl:'',
+            imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw4xHiYs4vnhBs9jqjYk0_JY3-SiSavqovXA&usqp=CAU",
             name:"Dancho",
             breed:"ulichna4",
             description:"Very Cute cat!4",
@@ -43,32 +40,41 @@ const server = http.createServer((req, res) => {
         let namePatern = /{{name}}/g
         let breedPatern = /{{breed}}/g
         let descriotionPatern = /{{description}}/g
+    const catTemplate = await fs.readFile('./views/home/catTemplte.html',"utf-8")
+    const homeHtml =await fs.readFile('./views/home/index.html',"utf-8")
+
 
         const catHtml = cats.map(cat => catTemplate
         .replace(imageUrlPatern, cat.imageUrl)
         .replace(namePatern, cat.name)
         .replace(breedPatern, cat.breed)
-        .replace(descriotionPatern, cat.description))
-       const homeHtmlTemplate =  homeHtml.replace(
-        "{{cats}}", catHtml)
+        .replace(descriotionPatern, cat.description)).join("")
+
+ 
+       const homeHtmlTemplate =  homeHtml.replace("{{cats}}", catHtml)
         res.writeHead(200, {
             "Content-Type": "text/html"
         })
         res.write(homeHtmlTemplate)
     }
     else if (url === '/content/styles/site.css') {
+        const siteCss =await fs.readFile('./content/styles/site.css',"utf-8")
+        console.log(siteCss)
         res.writeHead(200, {
             "Content-Type": "text/css"
         })
         res.write(siteCss)
+
     }
     else if (url === '/cats/add-breed') {
-        res.writeHead(200, {
-            "Content-Type": "text/html"
-        })
-        res.write(addBread)
+       const addBread =await fs.readFile('./views/home/addBreed.html',"utf-8")
+       res.writeHead(200, {
+        "Content-Type": "text/html"
+    })
+    res.write(addBread)
     }
     else if (url === '/cats/add-cat') {
+        const addCat =await fs.readFile('./views/home/addCat.html',"utf-8")
         res.writeHead(200, {
             "Content-Type": "text/html"
         })
